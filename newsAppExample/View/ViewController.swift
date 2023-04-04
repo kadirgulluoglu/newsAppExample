330ae48b6879b62146a589696ab924f76cbebf39
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-04-04 16:45:44 +0300

    show data

--- newsAppExample/View/ViewController.swift
+++ newsAppExample/View/ViewController.swift
@@ -4,7 +4,7 @@ import UIKit
 
 class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
   
-
+    private var newsTableViewModel: NewsTableViewModel?
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -12,16 +12,43 @@ class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSourc
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        
+        veriAl()
+       
+    }
+    
+    
+    func veriAl(){
+        let url = URL(string:  "https://raw.githubusercontent.com/atilsamancioglu/BTK-iOSDataSet/master/dataset.json")
+        
+        WebService().getNews(url: url!){ (news) in
+            if let news = news{
+                self.newsTableViewModel = NewsTableViewModel(newsList: news)
+                
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return newsTableViewModel?.numberOfRowsInSection() ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
-    
+        let newsViewModel = self.newsTableViewModel?.newsAtIndexPath(indexPath.row)
+        cell.titleLabel.text = newsViewModel?.title
+        cell.subtitleLabel.text = newsViewModel?.story
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 
 }
