96b2f46cae8debe15ffd5084f2726c09b53e9700
Kadir Güllüoğlu <kadirrbey001@gmail.com>
2023-04-04 16:45:35 +0300

    create service

--- newsAppExample/Service/NewsService.swift
+++ newsAppExample/Service/NewsService.swift
@@ -2,7 +2,20 @@
 import Foundation
 
 class WebService {
-    func getNews (url:URL, completion: @escaping () -> ([News]?)) {
-        
+    func getNews (url:URL, completion: @escaping ([News]?) -> ()) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+            else if let data = data {
+                let news = try? JSONDecoder().decode([News].self, from: data)
+                if let news = news {
+                    completion(news)
+                }
+                
+            }
+           
+        }.resume()
     }
 }
